c4fa5be100883f86d44dda765c3fcb638779302d
nominmar <nominmar@gmail.com>
2024-01-13 21:38:40 +0100

    input params

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <format>
+
+const int GRID_HEIGHT = 500;
+const int GRID_WIDTH = 500;
+
+class Player
+{
+    public:
+        int x=0;
+        int y=0;
+};
+
+
+
+
+int main(){
+    std::cout << "y";
+    return 0;
+}
