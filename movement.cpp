653940fc1c8c38a61f34d7fa6e47a9ee4d299020
nominmar <nominmar@gmail.com>
2024-01-14 10:33:26 +0100

    moving player definitions out

--- movement.cpp
+++ movement.cpp
@@ -3,43 +3,7 @@
 #include <ctime>
 #include <tuple>
 #include "movement.h"
-
-class Point
-{
-
-private:
-    int grid_limit = 500;
-
-public:
-    int x, y;
-    Point(int x, int y) : x(x), y(y) {}
-
-    int setGridLimit(const int limit)
-    {
-        grid_limit = limit;
-    }
-
-    void move(int axis, int direction)
-    {
-        if (axis == 0)
-        {
-            x += direction;
-            if (x < 0 || x > grid_limit)
-            {
-                x -= direction;
-            }
-        }
-        else if (y < grid_limit)
-        {
-            y += direction;
-            if (y < 0 || y > grid_limit)
-            {
-                y -= direction;
-            }
-            
-        }
-    }
-};
+#include "players.h"
 
 std::pair<int, int> randomMoveParams()
 {
